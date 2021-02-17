5a98181e88fdc0147004357fb5b444467c914354
hegoog <34955068+hegoog@users.noreply.github.com>
2021-02-17 15:28:11 +0900

    Delete interpark directory

--- interpark/src/main/java/kr/kosmo/jobkorea/commonbiz/service/MsgMgmtService.java
+++
@@ -1,9 +0,0 @@
-package kr.kosmo.jobkorea.commonbiz.service;
-
-import java.util.Map;
-
-public interface MsgMgmtService {
-
-	/** 메일 발송 */
-	public void sendMsg(Map<String, Object>paramMap) throws Exception; 
-}
